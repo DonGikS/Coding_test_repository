a3c1e21c69e243b039910055334d06d75d9da445
DonGikS <ikspurt0505@gmail.com>
2020-08-12 17:38:38 +0900

    greedy and brut_force file

---
+++ brut_force/big.py
@@ -0,0 +1,29 @@
+def main():
+    number_of_man = int(input())
+    if number_of_man > 50 or number_of_man < 2:
+        return
+
+    human_resource = []
+    for i in range(number_of_man):
+        a_list = []
+        a, b = map(int, input().split(' '))
+        if a < 10 or a > 200 or b < 10 or b > 200:
+            return
+        else:
+            a_list.append(a)
+            a_list.append(b)
+        human_resource.append(a_list)
+
+    rank_list = []
+    for j in human_resource:
+        h_m = 0
+        for i in range(len(human_resource)):
+            if (j[0] != human_resource[i][0]) and (j[1] != human_resource[i][1]):
+                if(j[0] < human_resource[i][0]) and (j[1] < human_resource[i][1]):
+                    h_m += 1
+        rank_list.append(h_m+1)
+
+    for i in rank_list:
+        print(i, end=' ')
+
+main()
---
+++ greedy/inha_atm_greedy.py
@@ -0,0 +1,19 @@
+def main():
+    num = int(input())
+    if (num < 1) or (num > 1000):
+        return
+    a_list = list(map(int, input().split()))
+    if len(a_list) != num:
+        print('what')
+        return
+    for i in a_list:
+        if (i < 1) or (i > 1000):
+            print('no')
+            return
+    a_list.sort()
+    sum = 0
+    for i in range(len(a_list), 0, -1):
+        sum += i * a_list[len(a_list) - i]
+    print(sum)
+
+main()
